029b0e3d56f398b30a751844f46d7acaebff1105
Bjorn Pardo <bjorn@xend.com.ph>
2014-04-29 17:28:58 -0600

    changed class structure

--- main.js
+++ main.js
@@ -3,31 +3,32 @@
  *********************************/
 
 
-var Courier = function(name, capableAreas) {
+var Courier = function(name, capableAreas, defaultAreas) {
 	this.name = name;
  	this.capableAreas = capableAreas;
+ 	this.defaultAreas = defaultAreas;
  	this.daily = [];
 };
 
-var Area = function(name, defaultCourier) {
+var Area = function(name) {
 	this.name = name;
-	this.defaultCourier = defaultCourier;
+	
 };
 
-var courier1 = new Courier ('1', [area1, area2 , area3, area4]);
-var courier2 = new Courier ('2', [area4, area5, area6, area7]);
-var courier3 = new Courier ('3', [area1, area2 , area3, area4, area4, area5, area6, area7, area8, area9, area10]);
+var area1 = new Area ('a');
+var area2 = new Area ('b');
+var area3 = new Area ('c');
+var area4 = new Area ('d');
+var area5 = new Area ('e');
+var area6 = new Area ('f');
+var area7 = new Area ('g');
+var area8 = new Area ('h');
+var area9 = new Area ('i');
+var area10 = new Area ('j');
 
-var area1 = new Area ('a', courier1);
-var area2 = new Area ('b', courier1);
-var area3 = new Area ('c', courier1);
-var area4 = new Area ('d', courier1);
-var area5 = new Area ('e', courier2);
-var area6 = new Area ('f', courier2);
-var area7 = new Area ('g', courier2);
-var area8 = new Area ('h', courier3);
-var area9 = new Area ('i', courier3);
-var area10 = new Area ('j', courier3);
+var courier1 = new Courier ('1', [area1, area2 , area3, area4], [area1, area2, area3]);
+var courier2 = new Courier ('2', [area4, area5, area6, area7], [area4, area5]);
+var courier3 = new Courier ('3', [area1, area2 , area3, area4, area4, area5, area6, area7, area8, area9, area10], [area6, area7, area8, area9, area10]);
 
 var allAreas = [area1, area2 , area3, area4, area4, area5, area6, area7, area8, area9, area10];
 
@@ -45,20 +46,39 @@ var allCouriers = [courier1, courier2, courier3];
  * FUNCTION DEFINITIONS
  *********************************/
 
-// var couriersPresent = ;
+// Creating array of all couriers present base  d on who is checked
+function getValues() {
+	var couriersPresent = [];
 
-// $('.btn').on('click',function() {
-// 		// console.log("ok");
-// 		var quote = $("textarea.quote").val();
-// 		var author = $("input.author").val();
-// 		var star = $("input.star").val();
-// 		$('ul').append('<li class="result"> <span class="hidestar">' 
-// 			+ star + '</span>' + '" ' + quote +' "' + ' ~ ' + author 
-// 			+ '. (Star Rating: ' + star + ')</li>') ;
- 
-var assignAreas = function (couriersPresent, allAreas) {
+		$('input[type=checkbox]:checked').each(function() {
+        couriersPresent.push($(this).parent().text());
+        });
 
-}
+		return couriersPresent;
+};
+
+
+// Search for courier obj by name
+function searchCourier(name) {
+	for(var i = 0; i < allCouriers.length; i++) {
+		if (this.name === name) {
+			return allCouriers[i];
+		}
+	}
+};
+
+
+// function searchCouriers(arr) {
+// 	var presentCouriers = [];
+// 	for(var i = 0; i < allCouriers.length; i++) {
+// 		if (this.name = arr) {
+// 			presentCouriers.push('allCouriers[i]');
+// 			return presentCouriers;
+// 		}
+// 	}
+// };
+
+
 
 
 /*********************************
@@ -67,10 +87,26 @@ var assignAreas = function (couriersPresent, allAreas) {
 
 $(document).on('ready', function() {
 
-	for ( var i = 0; i < allCouriers.length, i++) {
-		var checkbox = $('<input type = "checkbox">');
+	for ( var i = 0; i < allCouriers.length; i++) {
+		
 		var label = $('<label>');
+		label.text(allCouriers[i].name);
+
+		var checkbox = $('<input type="checkbox" class="checkbox" checked>');
+
+		$('#courier-container').append(label);
+		label.prepend(checkbox);
+
+		// $('#courier-container').append(checkbox);
 	}
+
+	// Add Load button after checkboxes
+	$('#courier-container').append('<div><input type="button" value="Load" id="load-btn"></div>');
+
+	$('#load-btn').click(function() {
+        // getValues();
+        console.log(getValues());
+    });
   
 });
 
